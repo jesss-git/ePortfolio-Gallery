b61b433a222462b955d1f2e6cf6a56d4794db1af
jesss-git <jessicachan@Jessicas-MacBook-Pro.local>
2025-12-04 22:08:44 -0500

    Parallax

--- data/Gallery.jsx
+++ data/Gallery.jsx
@@ -56,7 +56,7 @@ export default function Gallery() {
     // scale & move entire photographer + lens container
     tl.to(heroBoxRef.current, {
       scale: 7,
-      y: -200,
+      y: -150,
       transformOrigin: "center center",
       ease: "none"
     }, 0);
